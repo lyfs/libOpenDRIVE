992b6d6f01ec4fae454348cf63811de73aa916a9
Sebastian Pagel <mail@sebastian-pagel.net>
2021-12-19 11:26:39 +0100

    format-files, exlude Viewer/glad and KHR

--- format-files.sh
+++ format-files.sh
@@ -6,4 +6,6 @@ find -E . -type f \
     -not -path "./Thirdparty/*" \
     -not -name './Viewer/libOpenDrive.js' \
     -not -path './Viewer/lib/*' \
+    -not -path './Viewer/glad/*' \
+    -not -path './Viewer/KHR/*' \
     -exec clang-format --style=file -i '{}' \;
